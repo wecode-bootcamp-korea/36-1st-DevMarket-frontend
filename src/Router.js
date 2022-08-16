d5b38a3729377ccd97e6a3b2a88c75464c5233c3
seoyul <seoyul0203@gmail.com>
2022-08-16 16:53:06 +0900

    장바구니 컴포넌트 추가

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cart from './pages/Cart/Cart';
 import DetailPage from './pages/DetailPage/DetailPage';
 import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
@@ -14,6 +15,7 @@ const Router = () => {
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/detail" element={<DetailPage />} />
+          <Route path="/cart" element={<Cart />} />
         </Route>
       </Routes>
     </BrowserRouter>
