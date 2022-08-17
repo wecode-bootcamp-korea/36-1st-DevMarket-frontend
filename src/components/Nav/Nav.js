ae1949a19a4deb0b280ab7eb04079e004d52f677
seoyul <seoyul0203@gmail.com>
2022-08-17 09:56:36 +0900

    Nav branch 생성

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './Nav.scss';
+import NavTop from './NavComponent/NavTop';
+// import { NavLink } from 'react-router-dom';
 
-function Nav() {
-  return <div>Nav</div>;
-}
+const Nav = () => {
+  return (
+    <nav className="nav_Main">
+      <div className="nav_Inner">
+        <NavTop />
+      </div>
+      {/* <div>
+        <NavLink to="/">Home</NavLink>
+      </div>
+      <div>
+        <NavLink to="/recommend">Recommend</NavLink>
+      </div>
+      <div>
+        <NavLink to="/search">Search</NavLink>
+      </div>
+      <div>
+        <NavLink to="/mypage">MyPage</NavLink>
+      </div> */}
+    </nav>
+  );
+};
 
 export default Nav;
---
+++ src/components/Nav/NavComponent/NavTop.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavTop.scss';
+
+function NavTop() {
+  return (
+    <div className="nav_Top">
+      <div className="nav_TopLeft">
+        <ul>
+          <li><li/>
+          <li><li/>
+          <li><li/>
+          <li><li/>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default NavTop;
