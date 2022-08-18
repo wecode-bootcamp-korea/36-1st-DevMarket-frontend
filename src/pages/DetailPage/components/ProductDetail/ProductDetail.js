1ab9409daad0cd439257212a2dbba4411996ed25
WH <wonho@WHui-MacBookPro.local>
2022-08-18 21:12:45 +0900

    Modify: 상세정보 사진 수정

--- src/pages/DetailPage/components/ProductDetail/ProductDetail.js
+++ src/pages/DetailPage/components/ProductDetail/ProductDetail.js
@@ -3,7 +3,10 @@ import '../ProductDetail/ProductDetail.scss';
 function ProductDetail() {
   return (
     <div className="productDetail">
-      <img src="./images/detail.jpg" alt="detail" />
+      <img src="./images/1.jpg" alt="detail" />
+      <img src="./images/2.jpg" alt="detail" />
+      <img src="./images/3.jpg" alt="detail" />
+      <img src="./images/4.jpg" alt="detail" />
     </div>
   );
 }
