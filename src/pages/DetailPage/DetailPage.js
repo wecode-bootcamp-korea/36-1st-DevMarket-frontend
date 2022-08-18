11bb44d984a1dd67ae3b6e23fa49483adf480582
WH <wonho@WHui-MacBookPro.local>
2022-08-18 10:28:31 +0900

    ADD: 상품상세 페이지 UI 작업중

--- src/pages/DetailPage/DetailPage.js
+++ src/pages/DetailPage/DetailPage.js
@@ -1,45 +1,83 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import './DetailPage.scss';
 
 function DetailPage() {
   return (
     <div className="main-wrap">
-      <div className="top-margin">
-        <section className="inner-container">
-          <div className="left-col">
-            <section className="product-img">
-              <div />
-              <img src="./images/coffee.jpg" alt="coffee" />
-            </section>
-            <div className="productDetailWrap">
-              <div className="detailNavBar">
-                <div className="navTab">
-                  <span>상품 상세</span>
-                </div>
-                <div className="navTab">
-                  <span>상품 후기 (207)</span>
-                </div>
-                <div className="navTab">
-                  <span>1:1 문의</span>
-                </div>
+      <section className="inner-container">
+        <div className="left-col">
+          <section className="product-img">
+            <div />
+            <img src="./images/duck.jpg" alt="coffee" />
+          </section>
+          <div className="productDetailWrap">
+            <div className="detailNavBar">
+              <div className="navTab">
+                <span>상품 상세</span>
               </div>
-              <div className="productDetail">
-                <img src="./images/detail.jpg" alt="detail" />
+              <div className="navTab">
+                <span>상품 후기 (207)</span>
+              </div>
+              <div className="navTab">
+                <span>1:1 문의</span>
               </div>
             </div>
+            <div className="productDetail">
+              <img src="./images/detail.jpg" alt="detail" />
+            </div>
           </div>
-          <div className="right-col">
-            <section className="section-top">
-              <h1 className="product-name">[든든] 스타벅스 커피 1kg</h1>
-              <div className="ship-card">
-                <div className="inner-card">
-                  <p>든든배송</p>
+        </div>
+        <div className="right-col">
+          <section className="section-top">
+            <h1 className="product-name">[든든] 오리 장난감</h1>
+            <div className="ship-card">
+              <div className="inner-card">
+                <div className="productInfo">
+                  <ul>
+                    <li>중량</li>
+                    <li>5.5kg</li>
+                  </ul>
+                </div>
+                <div className="productInfo">
+                  <ul>
+                    <li>제공 수량</li>
+                    <li>1ea</li>
+                  </ul>
+                </div>
+                <div className="productInfo">
+                  <ul>
+                    <li>기준 단가</li>
+                    <li>1kg 당 900원</li>
+                  </ul>
                 </div>
               </div>
-            </section>
-          </div>
-        </section>
-      </div>
+            </div>
+          </section>
+          <section className="section-bottom">
+            <div className="quantityUI">
+              <span>주문수량</span>
+              <div className="quantityBar">
+                <button className="handleQuantity">
+                  <FontAwesomeIcon icon={faMinus} />
+                </button>
+                <input value="1" />
+                <button className="handleQuantity">
+                  <FontAwesomeIcon icon={faPlus} />
+                </button>
+              </div>
+            </div>
+            <div className="costInfo">
+              <span>총 합계금액</span>
+              <div className="costInfo-right">
+                <span>1kg 당 900원</span>
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
     </div>
   );
 }
