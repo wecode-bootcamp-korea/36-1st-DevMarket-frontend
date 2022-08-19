ac6184a3eccb242fb90a91b503cde02ade456afb
WH <wonho@WHui-MacBookPro.local>
2022-08-19 19:46:37 +0900

    ADD: 리뷰 작성 기능 구현중

--- src/pages/DetailPage/components/ProductReview/ProductReview.js
+++ src/pages/DetailPage/components/ProductReview/ProductReview.js
@@ -1,19 +1,47 @@
 import { useState, useEffect } from 'react';
 import SingleReview from './SingleReview/SingleReview';
+import WriteReview from './WriteReview/WriteReview';
 import '../ProductReview/ProductReview.scss';
 
 function ProductReview() {
-  const [reviews, setReviews] = useState([]);
+  const [reviewList, setReviewList] = useState([]);
+  const [review, setReview] = useState([]);
+  const [reviewArray, setReviewArray] = useState([]);
+
+  const submitReview = event => {
+    event.preventDefault();
+    if (review === '') {
+      return;
+    }
+    setReviewArray([review, ...reviewArray]);
+    setReview('');
+  };
+
+  const handleReviewInput = event => {
+    setReview(event.target.value);
+  };
 
   useEffect(() => {
     fetch('./data/reviews.json')
       .then(res => res.json())
-      .then(result => setReviews(result));
+      .then(setReviewList);
   }, []);
 
   return (
     <div className="productReview">
-      {reviews.map(review => (
+      <form className="writeReviewWrapper" onSubmit={submitReview}>
+        <textarea
+          className="writeReview"
+          placeholder="내용을 입력해주세요"
+          onChange={handleReviewInput}
+          value={review}
+        />
+        <button className="submitReview">등록하기</button>
+      </form>
+      {reviewArray.map(review => (
+        <WriteReview key={review.index} {...review} />
+      ))}
+      {reviewList.map(review => (
         <SingleReview key={review.id} {...review} />
       ))}
     </div>
---
+++ src/pages/DetailPage/components/ProductReview/WriteReview/WriteReview.js
@@ -0,0 +1,15 @@
+import '../WriteReview/WriteReview.scss';
+
+function WriteReview({ review }) {
+  <section className="singleReview">
+    <div className="reviewer">
+      <span className="name">정**</span>
+      <span className="time">2022.08.19</span>
+    </div>
+    <div className="reviewContent">
+      <span className="content">{review}</span>
+    </div>
+  </section>;
+}
+
+export default WriteReview;
