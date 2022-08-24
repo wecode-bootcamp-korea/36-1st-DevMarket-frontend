ca529ec751214f179706fdb2ba9f669cd60dc532
seoyul <seoyul0203@gmail.com>
2022-08-24 10:52:05 +0900

    수정사항 반영 완료

--- src/components/Nav/NavComponent/NavTop/NavTop.js
+++ src/components/Nav/NavComponent/NavTop/NavTop.js
@@ -6,23 +6,39 @@ function NavTop() {
   return (
     <nav className="navTopWrap">
       <ul className="navUl">
-        <li className="list">배민사장님광장 &nbsp; |</li>
-        <li className="list">배민셀프서비스 &nbsp; |</li>
-        <li className="list">배민아카데미 &nbsp; |</li>
-        <li className="list">배민장부 &nbsp; |</li>
-        <li className="list">배민로봇</li>
+        {TOP_LEFT.map(list => {
+          return (
+            <li className="list" key={list.id}>
+              {list.list}
+            </li>
+          );
+        })}
       </ul>
       <ul className="navUl">
-        <li className="list">고객센터 &nbsp; |</li>
-        <li className="list">
-          <Link to="/login">로그인 &nbsp; |</Link>
-        </li>
-        <li className="list">
-          <Link to="/signup">회원가입 &nbsp; </Link>
-        </li>
+        {TOP_RIGHT.map(list => {
+          return (
+            <Link to={list.link} key={list.id}>
+              <li className="list">{list.list}</li>
+            </Link>
+          );
+        })}
       </ul>
     </nav>
   );
 }
 
 export default NavTop;
+
+const TOP_LEFT = [
+  { id: 1, list: ' 배민사장님광장 ' },
+  { id: 2, list: ' 배민셀프서비스 ' },
+  { id: 3, list: ' 배민아카데미 ' },
+  { id: 4, list: ' 배민장부 ' },
+  { id: 4, list: ' 배민로봇 ' },
+];
+
+const TOP_RIGHT = [
+  { id: 1, list: ' 고객센터 |', link: '/' },
+  { id: 2, list: ' 로그인 |', link: '/login' },
+  { id: 3, list: ' 회원가입 ', link: '/signup' },
+];
