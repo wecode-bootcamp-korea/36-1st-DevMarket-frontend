58650ce34745592495a3090bde0fb89facfa10c8
함정석 <hahmjeongseok@hamjeongseog-ui-MacBookAir.local>
2022-08-24 20:12:26 +0900

    머지부분 오류 삭제 및 conflict 해결

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Carousel from '../../components/Carousel/Carousel';
+import List from '../../components/List/List';
 import './Main.scss';
 
 function Main() {
   return (
     <div>
       <Carousel />
+      <List />
     </div>
   );
 }
